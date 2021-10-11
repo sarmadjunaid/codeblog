17b30784f24330e30e711110ba129d31b289ff9c
sarmadjunaid <sarmad.junaid1997@gmail.com>
2021-10-11 20:52:19 +0500

    Added views and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
+
+def frontpage(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'frontpage.html', context)
